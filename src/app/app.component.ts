bb2e86faa80548613eff4057e1e5d12718e950ec
DSAghicha <dsaghicha211@gmail.com>
2022-05-29 11:05:33 +0530

    Function to open dialog box

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { TaskFormDialogComponent } from './task-form-dialog/task-form-dialog.component';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'angular_crud';
+    title = 'angular_crud';
+
+    constructor(private dialog: MatDialog) { }
+
+    openDialog() { 
+        this.dialog.open(TaskFormDialogComponent, {
+            width: '40%'
+        })
+    }
 }
