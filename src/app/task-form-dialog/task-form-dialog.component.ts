ddfc00b265b7b6d60807774c619acf08dd546306
DSAghicha <dsaghicha211@gmail.com>
2022-05-30 00:37:01 +0530

    Task Form Dialog Component Final

--- src/app/task-form-dialog/task-form-dialog.component.ts
+++ src/app/task-form-dialog/task-form-dialog.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+
+interface Category {
+    value: String
+    displayValue: String
+}
 
 @Component({
-  selector: 'app-task-form-dialog',
-  templateUrl: './task-form-dialog.component.html',
-  styleUrls: ['./task-form-dialog.component.sass']
+    selector: 'app-task-form-dialog',
+    templateUrl: './task-form-dialog.component.html',
+    styleUrls: ['./task-form-dialog.component.sass']
 })
 export class TaskFormDialogComponent implements OnInit {
+    taskForm !: FormGroup
 
-  constructor() { }
+    /** Static Category List */
+    categories: Category[] = [
+        {value: 'office', displayValue: 'Office'},
+        {value: 'personal', displayValue: 'Personal'}
+    ]
+    
+    constructor(private formBuilder: FormBuilder) { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void { 
+        this.taskForm = this.formBuilder.group({
+            taskTitle: ['', Validators.required],
+            taskDescription: ['', Validators.required],
+            taskCategory: ['', Validators.required],
+            taskDueDate: ['', Validators.required]
+        })
+    }
 
+    public addTask() {
+        console.log(this.taskForm.value)
+        
+    }
 }
